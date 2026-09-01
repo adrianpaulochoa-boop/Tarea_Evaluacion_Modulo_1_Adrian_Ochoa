fb156c1f0f9c80b5432962c415c757db89a066a9
Adrian Paúl Ochoa <adrianpaulochoa@gmail.com>
2026-09-01 14:08:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import estilos
 # Configuración global de la página
 st.set_page_config(
     page_title="Plataforma Oil & Gas",
-    page_icon="🛢️",
+    page_icon="⚙️",
     layout="wide",
     initial_sidebar_state="expanded"
 )
